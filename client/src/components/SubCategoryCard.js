e56814f7b9591fe611e913c9580c4722c1620c70
Devam Doshi <devamdoshi212@gmail.com>
2023-09-15 21:47:31 +0530

    add dynamic card

--- client/src/components/SubCategoryCard.js
+++ client/src/components/SubCategoryCard.js
@@ -1,11 +1,6 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Typography,
-  Avatar,
-} from "@material-tailwind/react";
-import monsoon from "./../assets/season/monsoon.jpg";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const StarIcon = () => {
   return (
     <svg
@@ -24,35 +19,54 @@ const StarIcon = () => {
 };
 
 const SubCategoryCard = () => {
+  const [result, setresult] = useState([]);
+  const urlparameter = useParams().subcategory;
+  console.log(urlparameter);
+  useEffect(() => {
+    const fetchsubcategorydata = async () => {
+      const Response = await fetch(
+        `http://localhost:9999/getReqCategory?Category=${urlparameter}`
+      );
+      if (!Response.ok) {
+        throw new Error("Something went Wrong");
+      }
+      const data = await Response.json();
+      setresult(data.data);
+      console.log(result);
+    };
+    fetchsubcategorydata().catch((e) => {
+      console.log(e);
+    });
+  }, [urlparameter]);
+
   return (
     <div class="flex justify-between gap-5 mx-16 mt-5">
-      <div class="w-1/2 relative rounded-lg overflow-hidden group">
-        <img src={monsoon} alt="" class="w-full h-60" />
-        <div class="absolute inset-0 flex items-center h-60 justify-center bg-black bg-opacity-50 transition-opacity duration-300 opacity-0 group-hover:opacity-100 group-hover:visible">
-          {/* <!-- Left-aligned heading and stars --> */}
-          <div class="w-full h-60 flex items-center justify-center p-2">
-            <h2 class="text-white text-2xl font-semibold ml-3">
-              Card 1 Heading
-            </h2>
-            <div class="flex items-center space-x-2 mr-3"></div>
+      {result.map((item) => (
+        <div class="w-1/2 relative rounded-lg overflow-hidden group">
+          <img src={item.Thumbnail} alt="" class="w-full h-60" />
+          <div class="absolute inset-0 flex items-center h-60 justify-center bg-black bg-opacity-50 transition-opacity duration-300 opacity-0 group-hover:opacity-100 group-hover:visible">
+            {/* <!-- Left-aligned heading and stars --> */}
+            <div class="w-full h-60 flex items-center justify-center p-2">
+              <h2 class="text-white text-2xl font-semibold ml-3">
+                {item.DestinationName}
+              </h2>
+              <div class="flex items-center space-x-2 mr-3"></div>
+            </div>
           </div>
-        </div>
-        {/* below content  */}
-        <div class=" absolute bottom-0 w-full flex items-start justify-between bg-transparent transition-opacity duration-300  p-2 opacity-100 visible group-hover:opacity-0 group-hover:invisible">
-          {/* <!-- Right-aligned heading and stars --> */}
-          <div class="flex items-center justify-between w-full p-2">
-            <h2 class="text-white text-2xl font-semibold ml-3">
-              Card 1 Heading
-            </h2>
-            <div className="flex items-center space-x-2 mr-3">
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
-              <StarIcon />
+          {/* below content  */}
+          <div class=" absolute bottom-0 w-full flex items-start justify-between bg-transparent transition-opacity duration-300  p-2 opacity-100 visible group-hover:opacity-0 group-hover:invisible">
+            {/* <!-- Right-aligned heading and stars --> */}
+            <div class="flex items-center justify-between w-full p-2">
+              <h2 class="text-white text-2xl font-semibold ml-3">
+                {item.DestinationName}
+              </h2>
+              <div className="flex items-center space-x-2 mr-3">
+                <StarIcon number={item.Rating} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
