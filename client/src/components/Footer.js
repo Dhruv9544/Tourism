a6f3aa3a22dc9f0dcd8fbaeba0109ea9785ec31a
Devam Doshi <devamdoshi212@gmail.com>
2023-09-24 13:53:08 +0530

    footer and home minute change

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,11 @@
 import { Typography } from "@material-tailwind/react";
 import { Link } from "react-router-dom";
 export const FooterWithLogo = () => {
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let currentDate = `${day}/${month}/${year}`;
   return (
     <footer className="w-full bg-[#252525] p-8">
       <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-[#252525]  text-center md:justify-between">
@@ -52,7 +57,10 @@ export const FooterWithLogo = () => {
       </div>
       <hr className="my-8 border-blue-gray-50" />
       <Typography color="white" className="text-center font-normal">
-        &copy; 2023 Tourism Tailwind
+        &copy; 2023 Tourism
+      </Typography>
+      <Typography color="white" className="text-center font-normal">
+        Last Updated : {currentDate}
       </Typography>
     </footer>
   );
