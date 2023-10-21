8581968df0bb95695dda670e1dc7d8a88237560d
Uday Gohel <goheluday6445@gmail.com>
2023-10-21 13:14:45 +0530

    About file

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,16 @@
 import React from "react";
+import pngwing2 from "./../assets/pngwing2.png";
 const About = () => {
   return (
-    <main>
-      <section className=" bg-lime-50 min-h-screen flex items-center justify-center">
+    <main className="relative">
+      <img
+        src={pngwing2}
+        alt="leaf"
+        className="absolute h-72 right-0 rotate-90 top-[-1.5rem] "
+      />
+      <img src={pngwing2} alt="leaf" className="absolute h-72 " />
+
+      <section className=" bg-lime-50 min-h-screen flex items-center justify-center ">
         <div className="container mx-auto ">
           <div className="flex flex-col sm:flex-row  ">
             {/* Left Side (Title, Description, Rating, and Visit Now) */}
@@ -75,90 +83,107 @@ const About = () => {
           />
         </div>
       </section>
-      <div class="container mx-auto p-8 max-w-full bg-lime-50 ">
-        <h1 class="text-3xl font-extrabold mb-4">History</h1>
+      <section className="bg-lime-50">
+        <div class="container mx-auto p-8 bg-lime-50">
+          <h1 class="text-3xl font-extrabold mb-4 [font-family:'Inter-SemiBold',Helvetica] uppercase">
+            History
+          </h1>
 
-        <p class="text-lg mb-6">
-          Gir National Park and Wildlife Sanctuary, also known as Sasan Gir, is
-          a forest, national park, and wildlife sanctuary near Talala Gir in
-          Gujarat, India. It is located 43 km (27 mi) north-east of Somnath, 65
-          km (40 mi) south-east of Junagadh and 60 km (37 mi) south-west of
-          Amreli.
-        </p>
-        <p class="text-lg mb-6">
-          Gir National Park is the only area, outside of Africa where you can
-          witness the majestic Asiatic Lion species in its native environment
-          along with the diverse variety of birds and small animals. It is a
-          significant dilemma for the maldharis, a tribe living inside the
-          forest since the livestock is often preyed upon by lions. The Panther
-          and the Hyena are the two such predators. Along with the long-tailed
-          langur, bear, Sambar, Chowsingha, Chital, Indian Deer, Chinkara,
-          Nilgai, and many others can also be found here.
-        </p>
-      </div>
-
-      <div class="container mx-auto px-4 py-8 bg-[#D3E4D0]  max-w-full">
-        <div class="flex flex-col md:flex-row justify-between">
-          <div class="flex-1">
-            <h1 class="text-3xl font-bold">Gir National Park</h1>
-            <p class="mt-4 text-gray-600">
-              Explore the majestic home of the Asiatic lions
-            </p>
-          </div>
-          <button class="mt-4 md:mt-0 bg-white text-black font-bold py-2 px-4 rounded shadow">
-            VISIT NOW
-          </button>
+          <li class="text-lg mb-6">
+            Gir National Park and Wildlife Sanctuary, also known as Sasan Gir,
+            is a forest, national park, and wildlife sanctuary near Talala Gir
+            in Gujarat, India. It is located 43 km (27 mi) north-east of
+            Somnath, 65 km (40 mi) south-east of Junagadh and 60 km (37 mi)
+            south-west of Amreli.
+          </li>
+          <li class="text-lg mb-6">
+            Gir National Park is the only area, outside of Africa where you can
+            witness the majestic Asiatic Lion species in its native environment
+            along with the diverse variety of birds and small animals. It is a
+            significant dilemma for the maldharis, a tribe living inside the
+            forest since the livestock is often preyed upon by lions. The
+            Panther and the Hyena are the two such predators. Along with the
+            long-tailed langur, bear, Sambar, Chowsingha, Chital, Indian Deer,
+            Chinkara, Nilgai, and many others can also be found here.
+          </li>
         </div>
+      </section>
 
-
-        {/* three boxes */}
-        <div class="flex flex-wrap justify-center">
-          <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4">
-            <h3>by bus </h3>
+      <section className="bg-[#D3E4D0] ">
+        {" "}
+        <div class="container mx-auto px-4 py-8">
+          <div class="flex flex-col md:flex-row justify-between">
+            <div class="flex-1">
+              <h1 class="text-3xl font-bold uppercase [font-family:'Inter-SemiBold',Helvetica]">
+                Gir National Park
+              </h1>
+              <p class="mt-4 font-light [font-family:'Inter-SemiBold',Helvetica] uppercase">
+                Explore the majestic home of the Asiatic lions
+              </p>
+            </div>
+            <button class="mt-4 md:mt-0 bg-white text-black font-bold py-2 px-4 rounded shadow">
+              VISIT NOW
+            </button>
           </div>
 
-          <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4">
-            <h3>by train</h3>
+          {/* three boxes */}
+          <div class="flex flex-wrap justify-between">
+            <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4 text-center">
+              <h3>By Bus </h3>
+            </div>
+
+            <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4 text-center">
+              <h3>By Train</h3>
+            </div>
+
+            <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4 text-center">
+              <h3>By Aeroplane</h3>
+            </div>
           </div>
 
-          <div class="w-[18rem] h-[18rem] flex-shrink-0 bg-gray-200 m-4">
-            <h3>by aeroplane</h3>
+          {/* timing , Entry fee , holiday */}
+          <div class="mt-8 [font-family:'Inter-SemiBold',Helvetica]">
+            <div class="mb-6 flex">
+              <h3 class="text-xl font-bold">Timing :</h3>
+              <p class="font-semibold ml-5 text-[#21825c] font-serif text-xl">
+                6:00 AM - 9:00 PM
+              </p>
+            </div>
+
+            <div class="mb-6 flex">
+              <h3 class="text-xl font-bold">Entry Fee :</h3>
+              <p class="font-serif text-xl font-semibold ml-5 text-[#21825c]">
+                ₹1000 per person
+              </p>
+            </div>
+
+            <div class="mb-6 flex">
+              <h3 class="text-xl font-bold">Holiday :</h3>
+              <p class="font-serif text-xl font-semibold ml-5">Sunday</p>
+            </div>
           </div>
-        </div>
 
-         {/* timing , Entry fee , holiday */}
-        <div class="mt-8 ">
-          <div class="mb-6 flex">
-            <h3 class="text-xl font-bold">Timing:</h3>
-            <p class="mt-1 ml">6:00 AM - 9:00 PM</p>
-          </div>
-
-          <div class="mb-6 flex">
-            <h3 class="text-xl font-bold">Entry Fee:</h3>
-            <p class="mt-1 ml-1">₹1000 per person</p>
-          </div>
-
-          <div class="mb-6 flex">
-            <h3 class="text-xl font-bold">Holiday:</h3>
-            <p class="mt-1 ml-1">Sunday</p>
-          </div>
-        </div>
-
-
-          <div class="mt-4">
-            <h3 class="text-xl font-bold">Website</h3>
-            <a href="gir.com" class="mt-2 text-blue-600 underline">
+          <div class="mt-4 flex [font-family:'Inter-SemiBold',Helvetica]">
+            <h3 class="text-xl font-bold">Website : </h3>
+            <a
+              href="gir.com"
+              class="ml-5  underline font-serif text-xl font-semibold text-[#21825c]"
+            >
               gir.com
             </a>
           </div>
 
-          <div class="mt-4">
-            <h3 class="text-xl font-bold">Quick Glimpse</h3>
-            <a href="youtube.com" class="mt-2 text-blue-600 underline">
+          <div class="mt-4 flex [font-family:'Inter-SemiBold',Helvetica]">
+            <h3 class="text-xl font-bold">Quick Glimpse : </h3>
+            <a
+              href="youtube.com"
+              class="ml-5 text-[#21825c] font-serif text-xl font-semibold underline"
+            >
               youtube.com
             </a>
           </div>
         </div>
+      </section>
     </main>
   );
 };
